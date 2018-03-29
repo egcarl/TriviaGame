62bb96b03ebb40763e3bb336b989e93e89debb4d
Eric Carl <egcarl@gmail.com>
2018-03-28 21:22:00 -0700

    I give up

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+var correctAnswers = 0
+var incorrectAnswers = 0 
+var timer = 0
+
+
+var questionBank = [
+    question1 = {
+        question : "What is the number one?",
+        choices : ["1", "2", "3", "4"],
+        answer : 0,
+    },
+    question2 = {
+        question : "What is the number four?",
+        choices : ["1", "2", "3", "4"],
+        answer : 3,
+    }
+];
+
+$("#start-button").click(function() {
+    console.log("I do nothing")
+    var questionHolder;
+    var ask;
+    var answers;
+    for (i = 0; i < questionBank.length; i++) {
+        console.log("i should be two")
+        
+    }
+
+})
